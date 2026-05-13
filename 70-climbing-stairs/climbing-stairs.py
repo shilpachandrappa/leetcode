e67cc696cbd7d7b567c2c3122e3d0702f828a8fb
shilpachandrappa <67675965+shilpachandrappa@users.noreply.github.com>
2026-05-12 21:24:04 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (16.86%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -5,9 +5,7 @@ class Solution:
         dp = [0] * (n+1)
         dp[1] = 1
         dp[2] = 2
-        for i in range(3, n+1):
+        for i in range(3, n+1) :
             dp[i] = dp[i-1] + dp[i-2]
         #print(dp)
-        return dp[n]
-        
-        
+        return dp[n] 
