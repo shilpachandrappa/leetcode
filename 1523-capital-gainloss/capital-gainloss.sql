69c8ef2d8b2fe75a814455bf4fbc989542980641
shilpachandrappa <67675965+shilpachandrappa@users.noreply.github.com>
2026-05-28 09:07:52 -0700

    Time: 512 ms (60.13%) | Memory: 0B (100.00%) - LeetSync

--- 1523-capital-gainloss/capital-gainloss.sql
+++ 1523-capital-gainloss/capital-gainloss.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-
+/*
 WITH cte AS (
     SELECT stock_name,operation, SUM(price) AS cost FROM Stocks 
     GROUP BY stock_name, operation
@@ -10,3 +10,9 @@ cte2 as (
 )
 SELECT stock_name , capital_gain_loss FROM cte2 
 where capital_gain_loss is not null
+*/
+
+SELECT stock_name, 
+SUM(IF(operation='Sell',price, 0)) - SUM(IF(operation='Buy',price,0)) as capital_gain_loss
+FROM Stocks
+GROUP BY stock_name
